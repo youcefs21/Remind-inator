f9980e1b54a278a44641a792644edae311f1b941
youcefs21 <youcefs2121@gmail.com>
2022-06-20 09:42:09 -0400

    reformat sqlSetup.py

--- sqlSetup.py
+++ sqlSetup.py
@@ -1,23 +1,35 @@
 import sqlite3
 import csv
 
-# Create and open Database
-conn = sqlite3.connect('TODO.db')
-c = conn.cursor()
 
-# Create table
-c.execute('''CREATE TABLE tasks
-             (task text, timeLeft int, priority int, subject text, timeUsed int)''')
+def setupDatabase():
+    # Create and open Database
+    conn = sqlite3.connect('TODO.db')
+    c = conn.cursor()
 
-# Open csv File and put data into data base
-with open("TODO.csv", "r") as file:
-    todo = csv.DictReader(file)
-    for row in todo:
-        c.execute('INSERT INTO tasks VALUES (?,?,?,?,?)',
-(row["task"],int(row["timeLeft"]),int(row["priority"]),row["subject"],int(row["timeUsed"])))
+    # Create table
+    c.execute('''CREATE TABLE tasks (task text, timeLeft int, priority int, subject text, timeUsed int)''')
 
-# commit the changes
-conn.commit()
+    # Open csv File and put data into data base
+    with open("TODO_prod.csv", "r") as file:
+        todo = csv.DictReader(file)
+        for row in todo:
+            c.execute(
+                'INSERT INTO tasks VALUES (?,?,?,?,?)', (
+                          row["task"],
+                          int(row["timeLeft"]),
+                          int(row["priority"]),
+                          row["subject"],
+                          int(row["timeUsed"])
+                )
+            )
 
-# close the database after done using it
-conn.close()
+    # commit the changes
+    conn.commit()
+
+    # close the database after done using it
+    conn.close()
+
+
+if __name__ == '__main__':
+    setupDatabase()
